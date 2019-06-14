567cde7817e0dbbbaf05f1abe2d13bb680a8d9f5
mbanga-vs <malika.banga@vitaminshoppe.com>
2019-06-15 00:42:18 +0530

    fixed api url

--- src/js/globalVariable.js
+++ src/js/globalVariable.js
@@ -1,5 +1,5 @@
 export default {
-	API_URL: '/blob/master/dist/api/movies.json',
+	API_URL: 'https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs',
 	MONTHS: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 	EVENT_IMAGE_URL: 'https://in.bmscdn.com/events/moviecard/',
 };
